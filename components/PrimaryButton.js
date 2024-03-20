82f924ec9af774bc4fa053279ff4f33bc0ef3c48
Matthew Denune <mattdenune@gmail.com>
2024-03-20 08:55:23 -0400

    created Title component, managing colors globaly

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,5 +1,8 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
+
+import Colors from "../constants/colors";
+
 function PrimaryButton({ children, onPress }) {
 
   return (
@@ -11,7 +14,7 @@ function PrimaryButton({ children, onPress }) {
             ? [styles.buttonInnerContainer, styles.pressed]
             : styles.buttonInnerContainer
         }
-        android_ripple={{ color: "#640233" }}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
@@ -28,7 +31,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
   },
   buttonInnerContainer: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
