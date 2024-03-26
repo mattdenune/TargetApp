5e3104e1685bce662e6ad7ddffb23e8a6366dff3
Matthew Denune <mattdenune@gmail.com>
2024-03-26 07:33:22 -0400

    created GuessLogItem component for GameScreen FlatList

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -7,6 +7,7 @@ import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
 import { Ionicons } from "@expo/vector-icons";
+import GuessLogItem from "../components/game/GuessLogItem";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -63,6 +64,8 @@ function GameScreen({ userNumber, onGameOver }) {
     setGuessRounds((prevGuessRounds) => [newRndNum, ...prevGuessRounds]);
   }
 
+  const guessRoundsListLength = guessRounds.length;
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
@@ -87,7 +90,12 @@ function GameScreen({ userNumber, onGameOver }) {
       {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
       <FlatList
         data={guessRounds}
-        renderItem={(itemData) => <Text>{itemData.item}</Text>}
+        renderItem={(itemData) => (
+          <GuessLogItem
+            roundNumber={guessRoundsListLength - itemData.index}
+            guess={itemData.item}
+          />
+        )}
         keyExtractor={(item) => item}
       />
     </View>
